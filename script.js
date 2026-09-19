a074bb4a7a77db5891d2c78a3d8b28d3caf5a52c
Iris <75283241+irisz22@users.noreply.github.com>
2026-09-19 16:29:00 -0400

    added grid hovering & changing of size

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 const container = document.getElementById("container");
 const newDiv = document.createElement("div");
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const newDiv = document.createElement("div");
+const button = document.querySelector("button");
+let size = 16;
+let sideLength = 600;
 
-        newDiv.style.backgroundColor = "grey";
-        newDiv.style.height = '24px';
-        newDiv.style.width = '24px';
+button.addEventListener('click', () => {
+    size = Number(prompt("Enter number of squares per side:"));
+    if (size > 100) {
+        window.alert("The maximum is 100! Try again.");
+    } else {
+        console.log(size);
+        clearGrid();
+        createGrid(size);
+    }
+});
 
-        console.log("new div created");
-        container.appendChild(newDiv);
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const newDiv = document.createElement("div");
+            let side = sideLength / size;
+
+            newDiv.style.backgroundColor = "grey";
+            newDiv.style.height = `${side}px`;
+            newDiv.style.width = `${side}px`;
+
+            newDiv.addEventListener('mouseenter', () => {
+                //console.log("listening");
+                //newDiv.classList.add('hoverClass');
+                newDiv.style.backgroundColor = "pink";
+                //console.log("added");
+            });
+
+            console.log("new div created");
+            container.appendChild(newDiv);
+        }
     }
 }
+
+function clearGrid() {
+    container.replaceChildren();
+}
+
+createGrid(16);
